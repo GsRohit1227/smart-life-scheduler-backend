487b976287a84937e5f0d85b90be4ee48e4412d0
GsRohit1227 <245122750038@mvsrec.edu.in>
2026-02-27 16:57:14 +0530

    Fix CORS fully - allow Authorization header

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
